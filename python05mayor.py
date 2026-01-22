8338ed9477c5f1caa8af490e10c35fc0fed9daac
sebastiansanchezpolanco <sebastiansanchezpolanco@gmail.com>
2026-01-22 20:08:30 +0100

    cambio a el progrma

--- python05mayor.py
+++ python05mayor.py
@@ -6,7 +6,7 @@ num2=float(input())
 if (num1==num2):
     print("Los numeros son iguales")
 elif(num1>num2):
-    print("el primer numero es mayor que el segundo")
+    print("el numero mayor es el primero:", num1)
 else:
-    print("el primer numero es menor que el segundo")
+    print("el numero mayor es el segundo:", num2)
 print("Fin del programa")
