27e3e4805bf0384183bd82be523b28233064d59d
sebastiansanchezpolanco <sebastiansanchezpolanco@gmail.com>
2026-01-22 19:56:55 +0100

    nuevo programa para comparar numeros

---
+++ python05mayor.py
@@ -0,0 +1,12 @@
+print("Dame un numero")
+num1=float(input())
+print("Dame otro numero")
+num2=float(input())
+if (num1==num2):
+    print("Los numeros son iguales")
+else:
+    if(num1>num2):
+        print("el primer numero es mayor que el segundo")
+    else:
+        print("el primer numero es menor que el segundo")
+print("Fin del programa")
