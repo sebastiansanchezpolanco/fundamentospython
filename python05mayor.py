71a88a44a866f852bf7ae61bdfda6dce891b9117
sebastiansanchezpolanco <sebastiansanchezpolanco@gmail.com>
2026-01-22 20:05:10 +0100

    cambio programa de numero mayor

--- python05mayor.py
+++ python05mayor.py
@@ -1,12 +1,12 @@
+print("Numero mayor")
 print("Dame un numero")
 num1=float(input())
 print("Dame otro numero")
 num2=float(input())
 if (num1==num2):
     print("Los numeros son iguales")
+elif(num1>num2):
+    print("el primer numero es mayor que el segundo")
 else:
-    if(num1>num2):
-        print("el primer numero es mayor que el segundo")
-    else:
-        print("el primer numero es menor que el segundo")
+    print("el primer numero es menor que el segundo")
 print("Fin del programa")
