12a7b62036268ffcf34300b25a4623985998424d
sebastiansanchezpolanco <sebastiansanchezpolanco@gmail.com>
2026-01-22 19:42:19 +0100

    nuevos programas

---
+++ python03condicionales.py
@@ -0,0 +1,12 @@
+#Ahora se abordan los condicionales 
+print("Ejemplo de condicionales")
+print("Dime tu edad")
+edad=input()
+edad=int(edad)
+#Preguntamos si es mayor de edad
+if (edad>=18):
+    print("Es mayor de edad, super")
+else:
+    print("Eres menor de edad, fin del programa")
+#Despues del if
+print("Fin del programa")
---
+++ python04positivonegativo.py
@@ -0,0 +1,11 @@
+print("Positivo/Negativo/Cero")
+print("dame un numero")
+num = int(input())
+if (num>0):
+    print("Positivo")
+else:
+    if(num==0):
+        print("Cero")
+    else:
+        print("Negativo")
+print("Fin del progrma")
