fcdb18a01dea70429c72c4390653f92be6a85393
sebastiansanchezpolanco <sebastiansanchezpolanco@gmail.com>
2026-01-29 19:59:30 +0100

    actualizacion

--- libreria24validaciones.py
+++ libreria24validaciones.py
@@ -6,7 +6,7 @@ def letraDNI(dni):
     return letra
 def validacionisbn(isbn):
     sumatoria=0
-    while (isbn.isdigit()==False and len(isbn)!=10):
+    while (isbn.isdigit()==False or len(isbn)!=10):
         print("El ISBN debe ser solo numeros y tener 10 caracteres, este contiene otros caracteres")
         isbn=input("Introduzca el número ISBN: ")
     for i in range (len(isbn)):
