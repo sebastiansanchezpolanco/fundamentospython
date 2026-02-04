e81d237de9c192dd434380e049b9a062035465e2
sebastiansanchezpolanco <sebastiansanchezpolanco@gmail.com>
2026-02-04 17:00:37 +0100

    actua

--- ORACLE/test.sql
+++ ORACLE/test.sql
@@ -1,2 +1,21 @@
 select * from EMP;
-select APELLIDO, SALARIO*14 as SALARIO_ANUAL from EMP where COMISION>100000
+select APELLIDO, SALARIO*14+COMISION as SALARIO_ANUAL from EMP where COMISION>100000;
+select APELLIDO, SALARIO*14+COMISION as SALARIO_ANUAL from EMP where SALARIO*14+COMISION>750000;
+select APELLIDO, SALARIO*14+COMISION as SALARIO_ANUAL from EMP where SALARIO*14+COMISION>1000000;
+select * from EMP order by DEPT_NO, OFICIO;
+select * from ENFERMO;
+select * from ENFERMO where FECHA_NAC>'1/1/70';
+select * from ENFERMO where FECHA_NAC<'1/1/70' order by INSCRIPCION desc;
+select * from PLANTILLA;
+select * from PLANTILLA where TURNO ='M';
+select * from PLANTILLA where TURNO ='N';
+select * from PLANTILLA where SALARIO*12>1000000 and FUNCION='INTERINO';
+select * from DOCTOR where SALARIO*12>3000000;
+select * from PLANTILLA where TURNO ='M' and SALARIO*12>2000000 and SALARIO*12<2250000;
+select * from EMP where FECHA_ALT<'01/01/80' and FECHA_ALT>'12/12/82';
+select * from DEPT where LOC='MADRID' or LOC='BARCELONA';
+
+
+
+
+
